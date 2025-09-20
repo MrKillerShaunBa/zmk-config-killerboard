eaa9611fd9624c25a16f95730248080d72fbfde9
MrKillerShaunBa <122672193+MrKillerShaunBa@users.noreply.github.com>
2025-09-20 18:08:50 +0530

    Create physical_layouts.c

---
+++ config/boards/shields/killerboard/physical_layouts.c
@@ -0,0 +1,7 @@
+#include <zmk/keys.h>
+#include <zmk/behavior.h>
+
+const struct zmk_physical_layout killerboard = {
+    .rows = 5,  // change to your keyboard rows
+    .cols = 14, // change to your keyboard cols
+};
